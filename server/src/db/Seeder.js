4ad060023d14405b7783e06c67fd21ebf459b564
Martin Serna <martin.serna@gmail.com>
2023-07-20 15:27:47 -0400

    starter code

--- server/src/db/Seeder.js
+++ server/src/db/Seeder.js
@@ -3,12 +3,9 @@
 /* eslint-disable no-console */
 import { connection } from "../boot.js"
 
-import BookSeeder from "./seeders/BookSeeder.js"
-
 class Seeder {
   static async seed() {
     // include individual seed commands here
-    await BookSeeder.seed()
 
     console.log("Done!")
     await connection.destroy()
--- server/src/routes/api/v1/booksRouter.js
+++ server/src/routes/api/v1/booksRouter.js
@@ -1,46 +1,11 @@
 import express from "express"
-import objection from "objection"
-const { ValidationError } = objection
 
 import { Book } from "../../../models/index.js"
-import cleanUserInput from "../../../services/cleanUserInput.js"
 
 const booksRouter = new express.Router()
 
-booksRouter.get("/", async (req, res) => {
-  try {
-    const books = await Book.query()
-    res.json({ books })
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ errors: error })
-  }
-})
-
-booksRouter.get("/:id", async (req, res) => {
-  try {
-    const book = await Book.query().findById(req.params.id)
-    res.json({ book })
-  } catch (error) {
-    res.status(500).json({ errors: err })
-  }
-})
-
-booksRouter.post("/", async (req, res) => {
-  const { body } = req
-  const formInput = cleanUserInput(body)
-
-  try {
-    const book = await Book.query().insertAndFetch(formInput)
-    res.status(201).json({ book })
-  } catch (error) {
-    if (error instanceof ValidationError) {
-      res.status(422).json({ errors: error.data })
-    } else {
-      console.log(error)
-      res.status(500).json({ errors: error })
-    }
-  }
+booksRouter.get('/', async (req, res) => {
+  // your code here
 })
 
 export default booksRouter
